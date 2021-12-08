ab958fe4e613ba057461c32f7fc80ee6e5cd0e3b
Robin Helbig <helbig67@web.de>
2021-12-08 09:28:47 +0100

    Add parameterized tests

--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -1,33 +1,64 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.awt.geom.Point2D;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LineTest {
-    private Line line1 = new Line(new Point2D.Double(0,0), new Point2D.Double(5,5));
-    private Line line2 = new Line(new Point2D.Double(0,0), new Point2D.Double(1,1));
-    private Line line3 = new Line(new Point2D.Double(0,5), new Point2D.Double(5,0));
+    static TestCase[] testData(){
+        Line line1 = new Line(new Point2D.Double(0,0), new Point2D.Double(5,5));
+        Line line2 = new Line(new Point2D.Double(0,0), new Point2D.Double(1,1));
+        Line line3 = new Line(new Point2D.Double(0,5), new Point2D.Double(5,0));
+        Line line4 = new Line(new Point2D.Double(1,1), new Point2D.Double(1,2));
+        Line line5 = new Line(new Point2D.Double(3,4), new Point2D.Double(2,5));
 
-    @Test
-    void getIntersectionPoint() throws Line.NoIntersectionError {
-        assertThrows(
-                Line.NoIntersectionError.class,
-                () -> line1.getIntersectionPoint(line2),
-                "excepted line1.getIntersectionPoint(line2) to throw but it didn't"
-        );
-
-        assertEquals(new Point2D.Double(2.5,2.5), line1.getIntersectionPoint(line3));
+        return new TestCase[]{
+                new TestCase(line1, line2, null,null),
+                new TestCase(line1, line3, new Point2D.Double(2.5,2.5), 90.0),
+                new TestCase(line4, line5, new Point2D.Double(1.0,6.0), 45.0),
+        };
     }
 
-    @Test
-    void getIntersectionAngle() throws Line.NoIntersectionError{
-        assertThrows(
-                Line.NoIntersectionError.class,
-                () -> line1.getIntersectionAngle(line2),
-                "excepted line1.getIntersectionAngle(line2) to throw but it didn't"
-        );
+    @ParameterizedTest
+    @MethodSource(value =  "testData")
+    void getIntersectionPoint(TestCase testCase) throws Line.NoIntersectionError {
+        if(testCase.intersectionPoint == null){
+            assertThrows(
+                    Line.NoIntersectionError.class,
+                    () -> testCase.line1.getIntersectionPoint(testCase.line2),
+                    "excepted line1.getIntersectionPoint(line2) to throw but it didn't"
+            );
+        } else {
+            assertEquals(testCase.intersectionPoint,  testCase.line1.getIntersectionPoint(testCase.line2));
+        }
+    }
 
-        assertEquals(90, line1.getIntersectionAngle(line3));
+    @ParameterizedTest
+    @MethodSource(value =  "testData")
+    void getIntersectionAngle(TestCase testCase) throws Line.NoIntersectionError{
+        if(testCase.intersectionAngle == null){
+            assertThrows(
+                    Line.NoIntersectionError.class,
+                    () -> testCase.line1.getIntersectionAngle(testCase.line2),
+                    "excepted line1.getIntersectionAngle(line2) to throw but it didn't"
+            );
+        } else {
+            assertEquals(testCase.intersectionAngle, testCase.line1.getIntersectionAngle(testCase.line2));
+        }
+    }
+
+    static class TestCase{
+        Line line1, line2;
+        Point2D intersectionPoint;
+        Double intersectionAngle;
+
+        TestCase(Line line1, Line line2, Point2D intersectionPoint, Double intersectionAngle){
+            this.line1 = line1;
+            this.line2 = line2;
+            this.intersectionPoint = intersectionPoint;
+            this.intersectionAngle = intersectionAngle;
+        }
     }
 }
